a55d8285d2a9b5ac999f8d35ff5f57a9cf463809
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-05-02 09:06:30 +0300

    Add models, connect database

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
+require("dotenv").config();
+const sequelize = require("./db");
 const router = require("./routes/router");
+const { Sequelize } = require("sequelize");
+const models = require("./model/models");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/dbname", (req, res) => res.send(process.env.DB_NAME));
 
 app.use("/", router);
 
-app.listen(3000);
+const connect = async () => {
+	try {
+		sequelize.authenticate();
+		sequelize.sync();
+		app.listen(PORT, () => console.log(`App started on port ${PORT}`));
+	} catch (e) {
+		console.log(e);
+	}
+};
+connect();
--- model/models.js
+++ model/models.js
@@ -1,2 +1,50 @@
 const sequelize = require("../db");
 const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("User", {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+		allowNull: false,
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+});
+
+const Comment = sequelize.define("Comment", {
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	text: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Video = sequelize.define("Video", {
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	vid: { type: DataTypes.STRING, allowNull: false },
+});
+
+User.hasMany(Video);
+Video.hasMany(Comment);
+User.hasMany(Comment);
+
+module.exports = {
+	User,
+	Video,
+	Comment,
+};
