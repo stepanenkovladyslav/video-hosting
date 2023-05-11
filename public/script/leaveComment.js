ffa11a6a6c9d7a73083111c1ebe05cb4578c812d
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-05-11 19:36:19 +0300

    Added notification about success or error when adding comment

--- public/script/leaveComment.js
+++ public/script/leaveComment.js
@@ -19,8 +19,24 @@ const submitComment = async (e) => {
 			videoId: videoId,
 		}),
 	});
-	const data = await res.json();
-	console.log(data);
+	if (!res.ok) {
+		const error = await res.json();
+		if (!document.querySelector(".error-message")) {
+			const errorMessage = document.createElement("p");
+			errorMessage.textContent = error.message;
+			errorMessage.className = "error-message";
+			commentForm.append(errorMessage);
+		}
+	} else {
+		if (document.querySelector(".error-message")) {
+			const error = document.querySelector(".error-message");
+			commentForm.removeChild(error);
+		}
+		const okMessage = document.createElement("p");
+		okMessage.className = "ok-message";
+		okMessage.textContent = "Successfully added comment";
+		commentForm.append(okMessage);
+	}
 };
 
 commentForm.addEventListener("submit", (e) => submitComment(e));
